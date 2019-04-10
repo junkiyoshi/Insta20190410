cbd7d9dfa7144f68e7c06458295240d1d0f10de4
中内 純 <nakauchi.k@g-hits.co.jp>
2019-04-10 18:14:18 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,105 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetLineWidth(2);
+	ofNoFill();
+	ofEnableDepthTest();
+
+	int len = 400;
+	int span = len * 0.1;
+	for (int x = len * -0.5; x < len * 0.5; x += span) {
+
+		for (int y = len * -0.5; y < len * 0.5; y += span) {
+
+			int z = len * 0.5;		
+			for (int i = 0; i < 4; i++) {
+			
+				auto rotation_y = glm::rotate(glm::mat4(), (float)(i * 90 * DEG_TO_RAD), glm::vec3(0, 1, 0));
+
+				vector<glm::vec3> rect;
+				rect.push_back(glm::vec4(x, y, z, 0) * rotation_y);
+				rect.push_back(glm::vec4(x + span, y, z, 0) * rotation_y);
+				rect.push_back(glm::vec4(x + span, y + span, z, 0) * rotation_y);
+				rect.push_back(glm::vec4(x, y + span, z, 0) * rotation_y);
+				this->base_rects.push_back(rect);
+			}
+
+			for (int i = 0; i < 2; i++) {
+
+				auto rotation_x = glm::rotate(glm::mat4(), (float)((90 + i * 180) * DEG_TO_RAD), glm::vec3(1, 0, 0));
+
+				vector<glm::vec3> rect;
+				rect.push_back(glm::vec4(x, y, z, 0) * rotation_x);
+				rect.push_back(glm::vec4(x + span, y, z, 0) * rotation_x);
+				rect.push_back(glm::vec4(x + span, y + span, z, 0) * rotation_x);
+				rect.push_back(glm::vec4(x, y + span, z, 0) * rotation_x);
+				this->base_rects.push_back(rect);
+			}
+		}
+	}
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	this->rects.clear();
+	for (auto& base_rect : this->base_rects) {
+
+		vector<glm::vec3> rect;
+		for (auto& point : base_rect) {
+
+			auto rotation_y = glm::rotate(glm::mat4(), ofMap(ofNoise(point.y * 0.001 + ofGetFrameNum() * 0.01), 0, 1, -PI, PI), glm::vec3(0, 1, 0));
+			glm::vec3 location = glm::vec4(point, 0) * rotation_y;
+
+			rect.push_back(location);
+		}
+
+		this->rects.push_back(rect);
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+
+	for (auto& rect : this->rects) {
+
+		vector<glm::vec3> fill_rect;
+		for (auto& point : rect) {
+
+			fill_rect.push_back(point * 0.99);
+		}
+
+		ofFill();
+		ofSetColor(239);
+		ofBeginShape();
+		ofVertices(fill_rect);
+		ofEndShape(true);
+
+		ofNoFill();
+		ofSetColor(39);
+		ofBeginShape();
+		ofVertices(rect);
+		ofEndShape(true);
+	}
+
+	this->cam.end();
+
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
